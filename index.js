836bfa403481a641d757672c31a510b616e9030f
redmerten <redmerten@yahoo.com>
2017-09-27 14:27:16 -0700

    initial app for heroku deploy

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-const express = require('express');
+const express = require('express')
 
 //this creates a new express app.  There can be many apps in a project.
 //but most only use one.
 //app sets up config to listen for routing requests
-const app = express();
+const app = express()
 
+//app object is the running express server
+//app.get creates a route handler
+//watch for a request from root route (localhost:5000/)
+//req comes from the visit to '/'
+//then response is sent
 app.get('/', (req, res) => {
-    res.send({ hi: 'there' });
-});
+    res.send({ hi: 'there' })
+})
 
-app.listen(5000);
+//heroku will pass runtime variables, if dev use 5000
+const PORT = process.env.PORT || 5000
+//express tells node to listen on port 5000
+app.listen(PORT)
