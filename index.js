d4b9b10b0bcf8d3d596d15dc9251c87628fbaeef
redmerten <redmerten@yahoo.com>
2018-01-02 15:40:52 -0800

    ready for heroku

--- index.js
+++ index.js
@@ -52,6 +52,16 @@ require('./routes/imageRoutes')(app)
 require('./routes/blogRoutes')(app)
 require('./routes/serviceRoutes')(app)
 
+if (process.env.NODE_ENV === 'production'){
+  app.use(express.static('client/build'))
+
+  const path = require('path')
+  app.get('*', (req, res)=>{
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
+
+
 //heroku will pass runtime variables, if dev use 5000
 const PORT = process.env.PORT || 5000
 //express tells node to listen on port 5000
